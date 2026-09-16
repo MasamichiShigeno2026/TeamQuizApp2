6ce449228154d211c6aad8b006810450d7a03adb
HidehikoAiba2026 <uni202607_05@telework.to>
2026-09-16 11:48:54 +0900

    UiUpdater.cs作成

---
+++ TeamQuizApp2/UiUpdater.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamQuizApp2
+{
+    /*******************************************/
+    /*      　　UiUpdaterクラスの定義  　     */
+    /*******************************************/
+    internal class UiUpdater
+    {
+        /*******************************************/
+        /*      　　　　フィールド　 　　 　　     */
+        /*******************************************/
+        private readonly Label _questionLabel;
+        private readonly Button[] _buttons;
+        private readonly ListBox _log;
+
+        /*******************************************/
+        /*      　　　コンストラクター 　 　　     */
+        /*******************************************/
+        public UiUpdater(Label questionLabel, Button[] buttons, ListBox log)
+        {
+            _questionLabel = questionLabel;
+            _buttons = buttons;
+            _log = log;
+        }
+
+        /*******************************************/
+        /*               メソッド                  */
+        /*******************************************/
+        // 引数で受け取った問題データ(q)を、実際の画面(_questionLabel.Text)に表示
+        public void ShowQuestion(Question q)
+        {
+            _questionLabel.Text = q.Text;
+            for (int i = 0; i < 4; i++)
+            {
+                _buttons[i].Text = q.Choices[i];
+            }
+        }
+
+        /*******************************************/
+        /*               メソッド                  */
+        /*******************************************/
+        // 結果(_log)を、画面のリストボックスに追加
+        public void LogResult(string msg)
+        {
+            _log.Items.Add(msg);
+        }
+    }
+}
